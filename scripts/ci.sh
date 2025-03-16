6a64a1b6784676dce164bfeaeab60658648d9622
Albert Albala <albert@mediatribe.net>
2025-03-15 23:56:41 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,7 +4,7 @@ set -e
 
 pwd
 ls -lah
-docker compose up -d
+docker compose -f docker-compose.yml up -d
 # https://github.com/dcycle/docker-html-validate
 find docs/_site -name "index.html" | xargs \
   docker run --rm -v "$(pwd)":/code dcycle/html-validate:3
