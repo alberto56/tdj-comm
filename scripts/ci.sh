b49c8b5c88bf799bba7c9cd1a2eb052a91161eaf
Albert Albala <albert@mediatribe.net>
2025-03-15 23:45:47 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,7 +2,7 @@
 # Runs the CI pipeline for the project
 set -e
 
-docker-compose up -d
+docker compose up -d
 # https://github.com/dcycle/docker-html-validate
 find docs/_site -name "index.html" | xargs \
   docker run --rm -v "$(pwd)":/code dcycle/html-validate:3
