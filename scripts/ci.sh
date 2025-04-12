d96aed7481c8d0da0b81a1821a9b67a03b092685
Albert Albala <albert@mediatribe.net>
2025-04-12 13:36:27 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,9 +8,10 @@ docker compose -f docker-compose.yml down
 docker network ls | grep tdj_comm || docker network create tdj_comm
 docker compose -f docker-compose.yml up -d
 sleep 3
+docker run --rm --network tdj_comm --entrypoint /bin/sh dcycle/html-validate:3 -c "apk add curl && curl -I http://jekyll:4000 && curl http://jekyll:4000"
 # https://github.com/dcycle/docker-html-validate
 docker run --rm --network tdj_comm dcycle/html-validate:3 http://jekyll:4000
-docker compose down
+docker compose -f docker-compose.yml down
 echo ""
 echo "Done!"
 echo ""
