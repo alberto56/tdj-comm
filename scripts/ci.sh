e91c9b1dc5f4a9ee509fd26b945924ab97144d44
Albert Albala <albert@mediatribe.net>
2025-04-12 13:28:54 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,9 +7,9 @@ ls -lah
 docker compose -f docker-compose.yml down
 docker network ls | grep tdj_comm || docker network create tdj_comm
 docker compose -f docker-compose.yml up -d
-sleep 4
+sleep 3
 # https://github.com/dcycle/docker-html-validate
-docker run --rm --network tdj_comm dcycle/html-validate:3 http://jekyll:4000 -F text
+docker run --rm --network tdj_comm dcycle/html-validate:3 http://jekyll:4000
 docker compose down
 echo ""
 echo "Done!"
