4fafb4f9b883f619d010236f0ee5ac3c78a4e4ab
Albert Albala <albert@mediatribe.net>
2025-04-12 14:14:26 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,11 +4,14 @@ set -e
 
 pwd
 ls -lah
-rm -rf ./docs/_site/*
-docker run -v ./docs:/srv/jekyll:rw --rm jekyll/minimal:3.8 jekyll build
-cat ./docs/_site/index.html
+rm -rf docs/_site
+docker compose -f docker-compose.yml down
+docker network ls | grep tdj_comm || docker network create tdj_comm
+docker compose -f docker-compose.yml up -d
+sleep 3
 # https://github.com/dcycle/docker-html-validate
 docker run --rm --network tdj_comm -v "$(pwd)":/code dcycle/html-validate:3 /code/docs/_site/index.html
+docker compose -f docker-compose.yml down
 echo ""
 echo "Done!"
 echo ""
