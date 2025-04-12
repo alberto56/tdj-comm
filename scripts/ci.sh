407e9c59957f4504e1b9799647551d499d269e38
Albert Albala <albert@mediatribe.net>
2025-04-12 14:22:41 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,7 +9,7 @@ mkdir -p ./docs/_site
 docker run --user root --rm -v ./docs:/srv/jekyll jekyll/minimal:3.8 jekyll build
 cat ./docs/_site/index.html
 # https://github.com/dcycle/docker-html-validate
-docker run --user root --rm --network tdj_comm -v "$(pwd)":/code dcycle/html-validate:3 /code/docs/_site/index.html
+docker run --user root --rm -v "$(pwd)":/code dcycle/html-validate:3 /code/docs/_site/index.html
 echo ""
 echo "Done!"
 echo ""
