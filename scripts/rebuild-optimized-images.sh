fe1ff2088e829cf1f00c3ab2fba5ecf5244b0a31
Suresh kumara <kumara.suresh25@gmail.com>
2025-05-13 20:15:26 +0530

    Update rebuild-optimized-images.sh

--- scripts/rebuild-optimized-images.sh
+++ scripts/rebuild-optimized-images.sh
@@ -7,4 +7,7 @@ set -e
 git clone https://github.com/dcycle/thumbor-example.git
 mv docs/media thumbor-example/my-media
 cd thumbor-example
+# Generate image mapping for media folder
 ./scripts/generate-image-map.sh ./app/my-media beta.terredesjeunes.org/media 800x,x200,x160,x200,75x75,600x680,x680 ./app/mapping-beta-tdj-comm.json
+# Generate image mapping for images folder
+./scripts/generate-image-map.sh ./app/my-images beta.terredesjeunes.org/images 800x,x200,x160,x200,75x75,600x680,x680 ./app/mapping-images-beta-tdj-comm.json
