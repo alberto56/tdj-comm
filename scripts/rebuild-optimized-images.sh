ded85e863660578b978b054937ab0ae182549215
Suresh kumara <kumara.suresh25@gmail.com>
2025-05-13 20:55:21 +0530

    Update rebuild-optimized-images.sh

--- scripts/rebuild-optimized-images.sh
+++ scripts/rebuild-optimized-images.sh
@@ -9,5 +9,3 @@ mv docs/media thumbor-example/my-media
 cd thumbor-example
 # Generate image mapping for media folder
 ./scripts/generate-image-map.sh ./app/my-media beta.terredesjeunes.org/media 800x,x200,x160,x200,75x75,600x680,x680 ./app/mapping-beta-tdj-comm.json
-# Generate image mapping for images folder
-./scripts/generate-image-map.sh ./app/my-images beta.terredesjeunes.org/images 800x,x200,x160,x200,75x75,600x680,x680 ./app/mapping-images-beta-tdj-comm.json
