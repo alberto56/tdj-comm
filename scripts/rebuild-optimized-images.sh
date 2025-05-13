5532cc46845b62e6c107a2a4713559a123995e35
Albert Albala <albert@mediatribe.net>
2025-05-13 13:21:16 -0400

    Allow image mapping (#138)

--- scripts/rebuild-optimized-images.sh
+++ scripts/rebuild-optimized-images.sh
@@ -8,4 +8,9 @@ git clone https://github.com/dcycle/thumbor-example.git
 mv docs/media thumbor-example/my-media
 cd thumbor-example
 # Generate image mapping for media folder
-./scripts/generate-image-map.sh ./app/my-media beta.terredesjeunes.org/media 800x,x200,x160,x200,75x75,600x680,x680 ./app/mapping-beta-tdj-comm.json
+./scripts/generate-image-map.sh \
+  ./app/my-media \
+  beta.terredesjeunes.org/media \
+  800x,x200,x160,x200,75x75,600x680,x680 \
+  ./app/mapping-beta-tdj-comm.json \
+  && echo "done"
