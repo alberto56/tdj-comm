3c3e6c2f6f9fa20c9b6a116ed88ad7866a157eaa
Albert Albala <albert@mediatribe.net>
2025-05-13 12:51:49 -0400

    Please squash this commit in the pull request

--- scripts/jenkins/rebuild-optimized-images.sh
+++ scripts/jenkins/rebuild-optimized-images.sh
@@ -45,7 +45,7 @@ scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@"$IP" "cd docker-tdj-images && export THUMBOR_SECURITY_KEY=$THUMBOR_SECURITY_KEY && ./scripts/install-docker-and-rebuild-optimized-images.sh"
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  root@"$IP":docker-tdj-images/thumbor-example/mapping-beta-tdj-comm.json mapping-2025.json
+  root@"$IP":docker-tdj-images/thumbor-example/mapping-beta-tdj-comm.json mapping-beta-tdj-comm.json
 cat mapping-beta-tdj-comm.json
 rm -rf tdj-image-mapping
 ssh-agent bash -c 'ssh-add "$TDJ_IMAGE_MAP_DEPLOY_PRIVATE_KEY"; git clone git@github.com:alberto56/tdj-image-mapping.git'
