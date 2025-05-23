3b01c0dd9d06d15300e9f2708752a4e931a6a505
Suresh kumara <kumara.suresh25@gmail.com>
2025-05-23 20:58:55 +0530

    I have create image map for media and images and later merged both the json files (#154)

--- scripts/rebuild-optimized-images.sh
+++ scripts/rebuild-optimized-images.sh
@@ -5,12 +5,15 @@
 set -e
 
 git clone https://github.com/dcycle/thumbor-example.git
+
+# mv media folder to thumbor-example
 mv docs/media thumbor-example/my-media
+
 cd thumbor-example
+
 # Generate image mapping for media folder
 ./scripts/generate-image-map.sh \
   ./app/my-media \
   beta.terredesjeunes.org/media \
-  800x,x680,x520,x451,x200,x160,75x75,x20 \
-  ./app/mapping-beta-tdj-comm.json \
-  && echo "done"
+  800x,x680,600x,604x200,x520,x451,x386,x368,x200,x160,120x120,100x100,75x75 \
+  ./app/unversioned/mapping-beta-tdj-comm.json
