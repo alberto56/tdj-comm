96ccd66c47765a357be4625b4e798231d39ee912
Albert Albala <albert@mediatribe.net>
2025-06-09 20:42:29 -0400

    2025-06-09-20:42:28

--- scripts/links.sh
+++ scripts/links.sh
@@ -6,5 +6,5 @@ docker kill tdj-apache-app || true
 docker rm tdj-apache-app || true
 echo ""
 echo "Vérification des liens brisés avec https://github.com/dcycle/docker-broken-link-checker"
-docker run -dit --name tdj-apache-app -p 8080:80 -v "$PWD"/docs/_site:/usr/local/apache2/htdocs/ httpd:2.4
-docker run --rm --link tdj-apache-app:site dcycle/broken-link-checker:3 http://site
+docker run -dit --name tdj-apache-app -p 8081:80 -v "$PWD"/docs/_site:/usr/local/apache2/htdocs/ httpd:2.4
+docker run --rm --link tdj-apache-app:site dcycle/broken-link-checker:3 -o text http://site
